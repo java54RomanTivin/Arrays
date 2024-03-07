ed5a5fe2fa14713cce99ad50ac5ce071209d70de
Yuragranovsky <yuragranovsky@gmail.com>
2024-03-07 21:03:57 +0200

    cw #5 ArrayChar initial methods & tests

--- src/telran/arrays/ArrayChar.java
+++ src/telran/arrays/ArrayChar.java
@@ -6,7 +6,27 @@ public class ArrayChar {
 	  this.array = array;
    }
    public int compareTo(ArrayChar another) {
-	   //TODO
-	   return -1;
+	   int length = Math.min(this.array.length, another.array.length);
+	   int index = 0;
+	   while(index < length && this.array[index] == another.array[index]) {
+		   index++;
+	   }
+	   return index == length ? this.array.length - another.array.length : 
+		   this.array[index] - another.array[index];
+   }
+   public int indexOf(char character) {
+	   int index = 0;
+	   while(index < array.length && array[index] != character) {
+		   index++;
+	   }
+	   return index == array.length ? -1 : index;
+	   
+   }
+   public int lastIndexOf(char character) {
+	   int index = array.length - 1;
+	   while(index > -1 && array[index] != character) {
+		   index--;
+	   }
+	   return index;
    }
 }
--- src/telran/arrays/test/ArrayCharTest.java
+++ src/telran/arrays/test/ArrayCharTest.java
@@ -10,7 +10,7 @@ class ArrayCharTest {
 
 	@Test
 	void compareToTest() {
-		char[] ar1 = {'1', '2', '3'};
+		char[] ar1 = {'1', '2', '3'}; //"123"
 		char[] ar2 = {'9'};
 		char[] ar3 = {'1', '2', '3'};
 		ArrayChar arrayChar1 = new ArrayChar(ar1);
@@ -23,6 +23,22 @@ class ArrayCharTest {
 		assertEquals(-1, new ArrayChar(new char[] {'a'})
 				.compareTo(new ArrayChar(new char[] {'a', 'a'})));
 		assertEquals(0, arrayChar1.compareTo(arrayChar3));
+	}
+	@Test
+	void indexOfTest() {
+		char[] ar1 = {'1', '2', '3', '2'}; //"123"
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		assertEquals(1, arrayChar1.indexOf('2'));
+		assertEquals(-1, arrayChar1.indexOf('9'));
+		
+	}
+	@Test
+	void lastIndexOfTest() {
+		char[] ar1 = {'1', '2', '3', '2'}; //"123"
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		assertEquals(3, arrayChar1.lastIndexOf('2'));
+		assertEquals(-1, arrayChar1.lastIndexOf('9'));
+		
 	}
 
 }
