eeb5cf9cdc51e05bdeb958f2583e5ac3f043a055
Yuragranovsky <yuragranovsky@gmail.com>
2024-03-11 16:24:35 +0200

    HW #5 ArrayChar

--- src/telran/arrays/ArrayChar.java
+++ src/telran/arrays/ArrayChar.java
@@ -1,18 +1,13 @@
 package telran.arrays;
 
+
 public class ArrayChar {
    private char[] array;
    public ArrayChar(char[] array) {
 	  this.array = array;
    }
    public int compareTo(ArrayChar another) {
-	   int length = Math.min(this.array.length, another.array.length);
-	   int index = 0;
-	   while(index < length && this.array[index] == another.array[index]) {
-		   index++;
-	   }
-	   return index == length ? this.array.length - another.array.length : 
-		   this.array[index] - another.array[index];
+	  return compareCommon(another, false);
    }
    public int indexOf(char character) {
 	   int index = 0;
@@ -29,4 +24,47 @@ public class ArrayChar {
 	   }
 	   return index;
    }
+   public int count(char character) {
+	   int result = 0;
+	   for(int i = 0; i < array.length; i++) {
+		   if(array[i] == character) {
+			   result++;
+		   }
+	   }
+	   return result;
+   }
+public boolean contains(char character) {
+	return indexOf(character) > -1;
+}
+public int compareToIgnoreCase(ArrayChar another) {
+	 return compareCommon(another, true);
+}
+private int compareCommon(ArrayChar another, boolean isIgnoreCase) {
+	 int length = Math.min(array.length, another.array.length);
+	   int index = 0;
+	   int diff = 0;
+	   while(index < length && (diff = getDiff(another, index, isIgnoreCase)) == 0) {
+		   index++;
+	   }
+	   return index == length ? this.array.length - another.array.length : 
+		   diff;
+}
+private int getDiff(ArrayChar another, int index, boolean isIgnoreCase) {
+	char first = array[index];
+	char second = another.array[index];
+	if (isIgnoreCase) {
+		first = Character.toLowerCase(first);
+		second = Character.toLowerCase(second);
+	}
+	return first - second;
+}
+public boolean equals(ArrayChar another) {
+	
+	return another != null && compareTo(another) == 0;
+}
+public boolean equalsIgnoreCase(ArrayChar another) {
+	
+	return another != null && compareToIgnoreCase(another) == 0;
+}
+
 }
--- src/telran/arrays/test/ArrayCharTest.java
+++ src/telran/arrays/test/ArrayCharTest.java
@@ -23,6 +23,7 @@ class ArrayCharTest {
 		assertEquals(-1, new ArrayChar(new char[] {'a'})
 				.compareTo(new ArrayChar(new char[] {'a', 'a'})));
 		assertEquals(0, arrayChar1.compareTo(arrayChar3));
+		
 	}
 	@Test
 	void indexOfTest() {
@@ -40,5 +41,75 @@ class ArrayCharTest {
 		assertEquals(-1, arrayChar1.lastIndexOf('9'));
 		
 	}
+	@Test
+	void countTest() {
+		char[] ar1 = {'1', '2', '3', '2'};
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		assertEquals(2, arrayChar1.count('2'));
+		assertEquals(1, arrayChar1.count('3'));
+		assertEquals(0, arrayChar1.count('a'));
+		
+	}
+	@Test
+	void compareToIgnoreCaseTest() {
+		char[] ar1 = {'B', 'E', 'N', 'A'};
+		char[] ar2 = {'a', 'l', 'n', 'a' };
+		char[] ar3 = {'b', 'e', 'n', 'a'};
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		ArrayChar arrayChar2 = new ArrayChar(ar2);
+		ArrayChar arrayChar3 = new ArrayChar(ar3);
+		assertEquals(1, arrayChar1.compareToIgnoreCase(arrayChar2));
+		assertEquals(-1, arrayChar2.compareToIgnoreCase(arrayChar1));
+		assertEquals(0, arrayChar1.compareToIgnoreCase(arrayChar3));
+		
+	}
+	@Test
+	void containsTest() {
+		char[] ar1 = {'1', '2', '3', '2'};
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		assertTrue(arrayChar1.contains('3'));
+		assertFalse(arrayChar1.contains('a'));
+		
+	}
+	@Test
+	void containsOutsideUpdateTest() {
+		char[] ar1 = {'1', '2', '3', '2'};
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		ar1[2] = 'a';
+		assertTrue(arrayChar1.contains('3'));
+		assertFalse(arrayChar1.contains('a'));
+	}
+	@Test
+	void equalsTest() {
+		char[] ar1 = {'B', 'E', 'N', 'A'};
+		char[] ar2 = {'B', 'E', 'N', 'A'};
+		char[] ar3 = {'b', 'e', 'n', 'a'};
+		char[] ar4 = {'B', 'E', 'N'};
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		ArrayChar arrayChar2 = new ArrayChar(ar2);
+		ArrayChar arrayChar3 = new ArrayChar(ar3);
+		ArrayChar arrayChar4 = new ArrayChar(ar4);
+		assertTrue(arrayChar1.equals(arrayChar2));
+		assertFalse(arrayChar1.equals(arrayChar3));
+		assertFalse(arrayChar1.equals(arrayChar4));
+		assertFalse(arrayChar1.equals(null));
+		
+	}
+	@Test
+	void equalsIgnoreCaseTest() {
+		char[] ar1 = {'B', 'E', 'N', 'A'};
+		char[] ar2 = {'B', 'E', 'N', 'A'};
+		char[] ar3 = {'b', 'e', 'n', 'a'};
+		char[] ar4 = {'B', 'E', 'N'};
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		ArrayChar arrayChar2 = new ArrayChar(ar2);
+		ArrayChar arrayChar3 = new ArrayChar(ar3);
+		ArrayChar arrayChar4 = new ArrayChar(ar4);
+		assertTrue(arrayChar1.equalsIgnoreCase(arrayChar2));
+		assertTrue(arrayChar1.equalsIgnoreCase(arrayChar3));
+		assertFalse(arrayChar1.equalsIgnoreCase(arrayChar4));
+		assertFalse(arrayChar1.equals(null));
+	}
+
 
 }
